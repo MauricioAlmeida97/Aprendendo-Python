5913a32cecf5b64a9bd216e5e3a091731e096988
JOSÉ MAURICIO DE SOUZA ALMEIDA <jmauricioalmeida97@gmail.com>
2024-05-14 20:53:44 -0300

    operadores de atribuicao

---
+++ Aula14.py
@@ -0,0 +1,13 @@
+"""
+Operadores de Atribuição
+ = += -= /= //= *= **= %=
+"""
+
+contador = 0
+contador += 1  # adiciona mais {valor}, neste caso 1
+contador -= 1  # subtrai um {valor} da variável, neste caso 1
+contador /= 1  # divide (float) do valor da variável
+contador //= 1 # divide (int)  do valor da variável 
+contador *= 1  # multiplica o valor da variável
+contador **= 1 # eleva o contador à potencia indicada
+ 
