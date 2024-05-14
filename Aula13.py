7e99f850eacde6d139ccc6bac255d06d844d53a6
JOSÉ MAURICIO DE SOUZA ALMEIDA <jmauricioalmeida97@gmail.com>
2024-05-14 20:19:26 -0300

    while-intro

---
+++ Aula13.py
@@ -0,0 +1,26 @@
+"""
+Repetições
+while (enquanto)
+Executa um ação enquanto uma condição for verdadeira
+"""
+"""EXEMPLO_1"""
+""" condicao = True
+
+while condicao:
+    nome = input("Digite seu nome: ")
+    print(f"Seu nome é {nome}")
+
+    if nome == 'sair':
+              break
+    
+print('acabou') """
+
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador = contador + 1
+    
+print("acabou")
+
+
